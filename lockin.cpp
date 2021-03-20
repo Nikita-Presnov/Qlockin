6f8761cc6fba8f4ee733304a48882188cdf6537e
nikita <presnovnikita@yandex.ru>
2021-03-20 18:08:40 +0300

    Inital commit

--- lockin.cpp
+++ lockin.cpp
@@ -23,50 +23,50 @@ int screach_lockin(char *idn, char *locname)
   for(int i=0;i<10;i++)
   {
     F_ID = open(comname, O_RDWR | O_NOCTTY);// | O_NONBLOCK);
-    if(F_ID == -1)
-    {
-       printf("It is not correct port\r");
-      }else{
-    struct termios options; /*структура для установки порта*/
-    tcgetattr(F_ID, &options); /*читает пораметры порта*/
-    cfsetispeed(&options, B9600); /*установка скорости порта*/
-    cfsetospeed(&options, B9600); /*установка скорости порта*/
-    options.c_cc[VTIME]    = 20; /*Время ожидания байта 20*0.1 = 2 секунды */
-    options.c_cc[VMIN]     = 0; /*минимальное число байт для чтения*/
+    if(F_ID == -1);
+//       printf("%s is not correct port\n", comname);
+    else{
+      struct termios options; /*структура для установки порта*/
+      tcgetattr(F_ID, &options); /*читает пораметры порта*/
+      cfsetispeed(&options, B9600); /*установка скорости порта*/
+      cfsetospeed(&options, B9600); /*установка скорости порта*/
+      options.c_cc[VTIME]    = 20; /*Время ожидания байта 20*0.1 = 2 секунды */
+      options.c_cc[VMIN]     = 0; /*минимальное число байт для чтения*/
 
-    options.c_cflag &= ~PARENB; /*бит четности не используется*/
-    options.c_cflag &= ~CSTOPB; /*1 стоп бит */
-    options.c_cflag &= ~CSIZE;  /*Размер байта*/
-    options.c_cflag |= CS8;  /*8 бит*/
+      options.c_cflag &= ~PARENB; /*бит четности не используется*/
+      options.c_cflag &= ~CSTOPB; /*1 стоп бит */
+      options.c_cflag &= ~CSIZE;  /*Размер байта*/
+      options.c_cflag |= CS8;  /*8 бит*/
 
-    options.c_lflag = 0;
-    options.c_oflag &= ~OPOST; /*Обязательно отключить постобработку*/
+      options.c_lflag = 0;
+      options.c_oflag &= ~OPOST; /*Обязательно отключить постобработку*/
 
-    options.c_iflag = 0;
-    options.c_iflag &= ~ (INLCR | IGNCR | ICRNL);
-
+      options.c_iflag = 0;
+      options.c_iflag &= ~ (INLCR | IGNCR | ICRNL);
 
 
-    tcsetattr(F_ID, TCSANOW, &options);
-    int n = write(F_ID, outx, 6);
-    n = write(F_ID, idns, 6);
-    for (int i=0 ;n!=0 && i<64;i++)
-    {
-      n = read(F_ID, &buff[i], 1);
-      if(buff[i] == '\r')
-        break;
-    }
-    close(F_ID);
-    if(strstr(buff, idn) &&
-       strstr(buff,"Stanford_Research_Systems,SR830,"))
-    {
-      for(int j=0; j < PORT_NAME_LEN; j++)
+
+      tcsetattr(F_ID, TCSANOW, &options);
+      int n = write(F_ID, outx, 6);
+      n = write(F_ID, idns, 6);
+      for (int i=0 ;n!=0 && i<64;i++)
         {
-          locname[j] = comname[j];
+          n = read(F_ID, &buff[i], 1);
+          if(buff[i] == '\r')
+            break;
         }
-      return 1;
+      close(F_ID);
+      if(strstr(buff, idn) && strstr(buff,"Stanford_Research_Systems,SR830,"))
+        {
+          for(int j=0; j < PORT_NAME_LEN; j++)
+            {
+              locname[j] = comname[j];
+            }
+          return 1;
+        }
+      else
+        printf("%s is not correct port\n", comname);
     }
-      }
     comname[number]++;
 //    if(comname[number]>'9' && number == 10)
 //    {
@@ -89,9 +89,9 @@ lockin::lockin(char *comname)
 {
   F_ID = open(comname, O_RDWR | O_NOCTTY);// | O_NONBLOCK);
   if(F_ID == -1)
-  {
-     printf("connection lost\n");
-  }
+    {
+       printf("connection lost\n");
+    }
   struct termios options; /*структура для установки порта*/
   tcgetattr(F_ID, &options); /*читает пораметры порта*/
   cfsetispeed(&options, B9600); /*установка скорости порта*/
@@ -111,10 +111,12 @@ lockin::lockin(char *comname)
   options.c_iflag &= ~ (INLCR | IGNCR | ICRNL);
   tcsetattr(F_ID, TCSANOW, &options);
 }
+
 int lockin::send_command(char *command)
 {
   return write(F_ID, command, strlen(command));
 }
+
 int lockin::get_data(char *command)
 {
   int n = write(F_ID, command, strlen(command));

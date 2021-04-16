548f95723a5fb7d116ada5c8b8eb80cd7c2af8f9
nikita <presnovnikita@yandex.ru>
2021-04-16 16:52:45 +0300

    New task for vscode.

--- lockin.h
+++ lockin.h
@@ -7,13 +7,13 @@ bool screach_lockin(char *idn, char *locname);
 class lockin
 {
 public:
-  lockin();
-  bool init(char *comname);
-  bool get_data();//char *command);
-  int send_command(char *command);
-  void close_lockin();
-  int F_ID;
-  char data[20];
+    lockin();
+    bool init(char *comname);
+    bool get_data();//char *command);
+    int send_command(char *command);
+    void close_lockin();
+    int F_ID;
+    char data[20];
 };
 
 #endif // LOCKIN_H

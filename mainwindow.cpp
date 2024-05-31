70ebbd9f0893c8c012a79d82497d2e8c0bcddd9f
Nikita <presnovnikita@yandex.ru>
2024-05-31 11:37:39 +0300

    Fix time

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,6 @@
+#define OFFLINE_DEBUG 1
+#define ENABLE_TIME 1
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFileDialog>
@@ -7,12 +10,11 @@
 #include <malloc.h>
 #include <time.h>
 
-MainWindow::MainWindow(QWidget *parent) :
-  QMainWindow(parent),
-  ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
+                                          ui(new Ui::MainWindow)
 {
-    char idn2[] = IDN2;//{'7','0','2','5','9'};
-    char idn1[] = IDN1;//{'8','1','5','9','5'};
+    char idn2[] = IDN2; //{'7','0','2','5','9'};
+    char idn1[] = IDN1; //{'8','1','5','9','5'};
     ui->setupUi(this);
     ui->textBrowser->setReadOnly(true);
     ui->stop_botton->setEnabled(false);
@@ -39,39 +41,38 @@ MainWindow::MainWindow(QWidget *parent) :
         QwtPlot::xBottom, QwtPlot::yLeft,
         QwtPlotPicker::CrossRubberBand,
         QwtPicker::ActiveOnly,
-        ui->qwtPlot_reference_r->canvas() );
-    d_picker1->setRubberBandPen( QColor( Qt::red ) );
-    d_picker1->setTrackerPen( QColor( Qt::black ) );
-    d_picker1->setStateMachine( new QwtPickerDragPointMachine() );
+        ui->qwtPlot_reference_r->canvas());
+    d_picker1->setRubberBandPen(QColor(Qt::red));
+    d_picker1->setTrackerPen(QColor(Qt::black));
+    d_picker1->setStateMachine(new QwtPickerDragPointMachine());
 
     QwtPlotPicker *d_picker2 = new QwtPlotPicker(
         QwtPlot::xBottom, QwtPlot::yLeft,
         QwtPlotPicker::CrossRubberBand,
         QwtPicker::ActiveOnly,
-        ui->qwtPlot_reference_signal->canvas() );
-    d_picker2->setRubberBandPen( QColor( Qt::red ) );
-    d_picker2->setTrackerPen( QColor( Qt::black ) );
-    d_picker2->setStateMachine( new QwtPickerDragPointMachine() );
+        ui->qwtPlot_reference_signal->canvas());
+    d_picker2->setRubberBandPen(QColor(Qt::red));
+    d_picker2->setTrackerPen(QColor(Qt::black));
+    d_picker2->setStateMachine(new QwtPickerDragPointMachine());
 
     cruve_reference_r = new QwtPlotCurve();
     cruve_reference_signal = new QwtPlotCurve();
     cruve_reference_r->setPen(Qt::blue, 3);
     QwtSymbol *symbol1 = new QwtSymbol(QwtSymbol::Ellipse,
-                                        QBrush( Qt::yellow ),
-                                        QPen( Qt::red, 0 ),
-                                        QSize( 4, 4 ) );
+                                       QBrush(Qt::yellow),
+                                       QPen(Qt::red, 0),
+                                       QSize(4, 4));
     cruve_reference_r->setSymbol(symbol1);
 
     cruve_reference_signal->setPen(Qt::green, 3);
     QwtSymbol *symbol2 = new QwtSymbol(QwtSymbol::Ellipse,
-                                        QBrush( Qt::blue ),
-                                        QPen( Qt::red, 0 ),
-                                        QSize( 4, 4 ) );
+                                       QBrush(Qt::blue),
+                                       QPen(Qt::red, 0),
+                                       QSize(4, 4));
     cruve_reference_signal->setSymbol(symbol2);
 
     cruve_reference_r->attach(ui->qwtPlot_reference_r);
     cruve_reference_signal->attach(ui->qwtPlot_reference_signal);
-
 
     ui->qwtPlot_reference_r->setAxisTitle(QwtPlot::yLeft, "R");
     ui->qwtPlot_reference_r->setAxisTitle(QwtPlot::xBottom, "Reference, 81595, mV");
@@ -81,33 +82,37 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->qwtPlot_reference_signal->setAxisTitle(QwtPlot::xBottom, "Reference, 81595, mV");
     ui->qwtPlot_reference_signal->replot();
 
-
-
     char name1[PORT_NAME_LEN];
     char name2[PORT_NAME_LEN];
+    loc2 = new lockin();
+    loc1 = new lockin();
+#ifndef OFFLINE_DEBUG
     screach_lockin(idn1, name1);
     screach_lockin(idn2, name2);
 
-    loc1 = new lockin();
-    if(!loc1->init(name1))
+    // loc1 = new lockin();
+    if (!loc1->init(name1))
     {
         QMessageBox msbox;
         msbox.setText("No locin with id 81595 for reference.");
         msbox.exec();
     }
-    loc2 = new lockin();
-    if(!loc2->init(name2))
+    // loc2 = new lockin();
+    if (!loc2->init(name2))
     {
         QMessageBox msbox;
         msbox.setText("No locin with id 70259 for signal.");
         msbox.exec();
     }
+#endif
 }
 
 MainWindow::~MainWindow()
 {
+#ifndef OFFLINE_DEBUG
     loc1->close_lockin();
     loc2->close_lockin();
+#endif
     // delete loc1;
     // delete loc2;
     // delete tmr;
@@ -120,47 +125,58 @@ MainWindow::~MainWindow()
 
 void MainWindow::updateval()
 {
-    char outr[7] = {'O','U','T','R','?','1','\r'};
-    loc1->send_command(outr);
-    loc2->send_command(outr);
-    #define BILLION  1000000000.0
+    char outr[7] = {'O', 'U', 'T', 'R', '?', '1', '\r'};
+
+#ifdef ENABLE_TIME
+#define BILLION 1000000000.0
     timespec meshtime;
-    timespec starttime;
-    if (progressframes==0)
+    static timespec starttime;
+    if (progressframes == 0)
     {
         clock_gettime(CLOCK_MONOTONIC_RAW, &starttime);
     }
-    
-    if(!loc1->get_data())
+#endif
+
+#ifndef OFFLINE_DEBUG
+    loc1->send_command(outr);
+    loc2->send_command(outr);
+    if (!loc1->get_data())
     {
         QMessageBox msbox;
         msbox.setText("No connection with locin 81595 (reference).");
         msbox.exec();
         progressframes = numberframes;
     }
-    if(!loc2->get_data())
+    if (!loc2->get_data())
     {
         QMessageBox msbox;
         msbox.setText("No connection with locin 70259 (signal).");
         msbox.exec();
         progressframes = numberframes;
     }
+#endif
+#ifdef ENABLE_TIME
     clock_gettime(CLOCK_MONOTONIC_RAW, &meshtime);
     double timenow = (double)(meshtime.tv_sec - starttime.tv_sec) + (double)(meshtime.tv_nsec - starttime.tv_nsec) / BILLION;
-    QTextStream outdata (&outputfile);
+#endif
+    QTextStream outdata(&outputfile);
 
     QString qdata1 = QString(loc1->data);
     QString qdata2 = QString(loc2->data);
+#ifdef ENABLE_TIME
     Timeval[progressframes] = timenow;
+#endif
     // printf("%f\n", timenow);
-    X[progressframes] = qdata1.toDouble()*1000;
-    Y1[progressframes] = qdata2.toDouble()*1000;
+    X[progressframes] = qdata1.toDouble() * 1000;
+    Y1[progressframes] = qdata2.toDouble() * 1000;
 
     ui->textBrowser->insertPlainText(QString::number(progressframes));
     outdata << progressframes << '\t';
-    // ui->textBrowser->insertPlainText(QString::fromStdString(" "));
-    // ui->textBrowser->insertPlainText(QString::number(timenow, 'g', 3));
-    // outdata << timenow << '\t';
+#ifdef ENABLE_TIME
+    ui->textBrowser->insertPlainText(QString::fromStdString(" "));
+    ui->textBrowser->insertPlainText(QString::number(timenow, 'g', 3));
+    outdata << timenow << '\t';
+#endif
     ui->textBrowser->insertPlainText(QString::fromStdString(" "));
     ui->textBrowser->insertPlainText(QString::number(X[progressframes], 'g', 3));
     outdata << X[progressframes] << '\t';
@@ -168,30 +184,28 @@ void MainWindow::updateval()
     ui->textBrowser->insertPlainText(QString::number(Y1[progressframes], 'g', 3));
     outdata << Y1[progressframes] << '\t';
     ui->textBrowser->insertPlainText(QString::fromStdString(" "));
-    if(X[progressframes]!=0)
+    if (X[progressframes] != 0)
     {
-        Y2[progressframes] = Y1[progressframes]/X[progressframes];
+        Y2[progressframes] = Y1[progressframes] / X[progressframes];
         ui->textBrowser->insertPlainText(QString::number(Y2[progressframes], 'g', 3));
         outdata << Y2[progressframes] << '\n';
     }
     else
     {
         // Y2[progressframes] = Y1[progressframes]/X[progressframes];
-        if (progressframes!=0)
+        if (progressframes != 0)
         {
-            Y2[progressframes] = Y2[progressframes-1];
+            Y2[progressframes] = Y2[progressframes - 1];
         }
         else
         {
-            Y2[progressframes]=0;
+            Y2[progressframes] = 0;
         }
         ui->textBrowser->insertPlainText(QString::fromStdString("inf"));
         outdata << "inf" << '\n';
     }
     ui->textBrowser->insertPlainText(QString::fromStdString("\n"));
     ui->textBrowser->moveCursor(QTextCursor::End);
-    
-    
 
     cruve_reference_r->setSamples(X, Y2, progressframes);
     cruve_reference_signal->setSamples(X, Y1, progressframes);
@@ -200,7 +214,7 @@ void MainWindow::updateval()
 
     ui->progresslcdNumber->display(progressframes);
     progressframes++;
-    if (numberframes<=progressframes)
+    if (numberframes <= progressframes)
     {
         progressframes = 0;
         tmr->stop();
@@ -219,9 +233,9 @@ void MainWindow::on_start_botton_clicked()
     filename.append(ui->prename->text());
     filename.append(QDateTime::currentDateTime().toString("_dd-MM-yyyy_HH-mm-ss"));
     filename.append(QString::fromStdString(".dat"));
-//   qDebug() << filename;
+    //   qDebug() << filename;
     outputfile.setFileName(filename);
-    if(!outputfile.open(QIODevice::WriteOnly))
+    if (!outputfile.open(QIODevice::WriteOnly))
     {
         QMessageBox msbox;
         msbox.setText("Error while touch file. No directory.");
@@ -231,9 +245,12 @@ void MainWindow::on_start_botton_clicked()
     {
         outputfile.write("n\ttime\treference\tsignal\tR\n");
         ui->textBrowser->clear();
-        // ui->textBrowser->insertPlainText(QString::fromStdString("N t Ref  Sig  R\n"));
+#ifdef ENABLE_TIME
+        ui->textBrowser->insertPlainText(QString::fromStdString("N t Ref  Sig  R\n"));
+#else
         ui->textBrowser->insertPlainText(QString::fromStdString("N Ref  Sig  R\n"));
-        period = (int)((ui->period_value->value())*1000);
+#endif
+        period = (int)((ui->period_value->value()) * 1000);
         tmr->setInterval(period);
         tmr->start();
         progressframes = 0;
@@ -241,11 +258,10 @@ void MainWindow::on_start_botton_clicked()
         ui->start_botton->setEnabled(false);
         ui->stop_botton->setEnabled(true);
         ui->dir_button->setEnabled(false);
-        X = (double *)malloc((4*numberframes)*sizeof(double));
+        X = (double *)malloc((4 * numberframes) * sizeof(double));
         Timeval = X + numberframes;
         Y1 = Timeval + numberframes;
         Y2 = Y1 + numberframes;
-
     }
 }
 
@@ -268,8 +284,9 @@ void MainWindow::on_dir_button_clicked()
 
 void MainWindow::on_rescan_button_clicked()
 {
-    char idn2[] = IDN2;//{'7','0','2','5','9'};
-    char idn1[] = IDN1;//{'8','1','5','9','5'};
+#ifndef OFFLINE_DEBUG
+    char idn2[] = IDN2; //{'7','0','2','5','9'};
+    char idn1[] = IDN1; //{'8','1','5','9','5'};
 
     char name1[PORT_NAME_LEN];
     char name2[PORT_NAME_LEN];
@@ -278,17 +295,18 @@ void MainWindow::on_rescan_button_clicked()
     screach_lockin(idn1, name1);
     screach_lockin(idn2, name2);
 
-    if(!loc1->init(name1))
+    if (!loc1->init(name1))
     {
         QMessageBox msbox;
         msbox.setText("No locin with id 81595 for reference.");
         msbox.exec();
     }
 
-    if(!loc2->init(name2))
+    if (!loc2->init(name2))
     {
         QMessageBox msbox;
         msbox.setText("No locin with id 70259 for signal.");
         msbox.exec();
     }
+#endif
 }
